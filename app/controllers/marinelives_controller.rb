67b7025caf06cf176668eba884221ab30f7f9630
srijakumar <srijakumar7@gmail.com>
2020-05-11 01:38:33 -0400

    Added index methods

--- app/controllers/marinelives_controller.rb
+++ app/controllers/marinelives_controller.rb
@@ -1,4 +1,10 @@
 class MarinelivesController < ApplicationController
+
+  def index
+      @marinelives = Marinelife.all
+      render json: @marinelives
+  end
+
     def create
         @marinelife = Marinelife.create(content: params[:marinelife], day_id: params[:day][:id])
         render json: @marinelife
